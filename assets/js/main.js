b93b55eba348195567b66b386cc78afff853ff76
alessio <alessiodg99.adg@gmail.com>
2020-12-14 16:17:30 +0100

    aggiunto immagine

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+let app = new Vue ({
+    el: "#app",
+    data: {
+        text: "Hello Vue",
+        url: "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg"
+    }
+});
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
